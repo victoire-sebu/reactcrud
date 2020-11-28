00ed5c56c0adef20299285659ec479b464ba37a2
victoire-sebu <vitechsebu@gmail.com>
2020-11-28 10:50:12 +0100

    Add component Mycars

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Mycars from './Components/Mycars';
 import './App.css';
 
 class App extends Component{
@@ -6,20 +7,7 @@ class App extends Component{
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Mycars/>
       </div>
     );
   }
---
+++ src/Components/Mycars.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Mycars extends Component{
+    render(){
+        return (
+            <div>
+                <h1>My cars</h1>
+            </div>
+        )
+    }
+}
+
+export default Mycars;
